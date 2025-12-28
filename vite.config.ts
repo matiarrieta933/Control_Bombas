c8f5254db866791a1c571c673d6705c31adb66ec
matiarrieta933 <matiarrieta05@gmail.com>
2025-12-28 02:24:41 -0300

    config: Agregar base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/Control_Bombas/',
   plugins: [react()],
   server: {
     host: true, // Permite acceso desde IP y resuelve problemas de localhost
